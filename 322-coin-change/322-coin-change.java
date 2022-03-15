5c86d66ee222163273f3a22966a3acd38ac1220e
rkk <30936750+rohankayan@users.noreply.github.com>
2022-03-14 18:14:18 -0700

    Time: 23 ms (68.60%), Space: 45 MB (51.85%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,35 +1,26 @@
 class Solution {
-    int numOfCoins = Integer.MAX_VALUE;
-    public int coinChange(int[] coins, int amount) {     
-      return  backtrack(coins,amount, new int[amount]);
-    
+    public int coinChange(int[] coins, int amount) {
+
+        int[] dp = new int[amount+1];
+       
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
         
-        
-    }
-    
-    
-    private int backtrack(int[] coins,  int remaining , int[] count ){
-        
-        if(remaining < 0 ){
-            return -1 ;
+        for(int a = 1; a<= amount; a++ ){            
+            for(int c: coins){
+                if(c<= a){
+                    dp[a] = Math.min(dp[a], dp[a-c]+1);
+                }
+            }
+            
+            
         }
         
-        if( remaining == 0){            
-            return 0;
-        }
-        if(count[remaining -1] !=0) return count[remaining -1];
-        
-        int min = Integer.MAX_VALUE;
-      for(int coin: coins){
-          
-          int temp = backtrack(coins,remaining-coin, count);
-          if (temp >=0 && temp < min)
-            min = temp +1;
-          
-      }
-      count[remaining -1] = min == Integer.MAX_VALUE ? -1 : min;
-      return count[remaining -1]; 
-        
-        
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
+
+    
+    
+    
+    
