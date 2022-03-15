6965956db534b8e55203aa47815844ba3a0a40a8
rkk <30936750+rohankayan@users.noreply.github.com>
2022-03-14 21:21:04 -0700

    Time: 32 ms (5.05%), Space: 59.2 MB (5.30%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -8,27 +8,19 @@ class Solution {
     
     private boolean isPalindrome(int l, int r, char[] c, int numOfMismatch){
         
-        if(l>=r ){
-            if(numOfMismatch > 1){
-                return false;
-            }
-            else{
-                //System.out.println(String.format("Ending Left: %s, Right:%s,numOfMismatch:%s ", l, r, numOfMismatch));
-                return true;
-            }
+        if(numOfMismatch > 1){
+            return false;
+        }
+        if(l>=r ){           
+            //System.out.println(String.format("Ending Left: %s, Right:%s,numOfMismatch:%s ", l, r, numOfMismatch));
+            return true;          
                 
         }
         
         if(c[l] != c[r]){
             //System.out.println(String.format("Unequal Left: %s, Right:%s,numOfMismatch:%s ", l, r, numOfMismatch));
-            if(numOfMismatch >= 1){
-                return false;
-            }
-            else{
-                
-                return isPalindrome(l+1, r,  c, numOfMismatch+1) || isPalindrome(l, r-1,  c,numOfMismatch+1); 
-            }            
-            
+            return isPalindrome(l+1, r,  c, numOfMismatch+1) || isPalindrome(l, r-1,  c,numOfMismatch+1); 
+
         }
         else{
             //System.out.println(String.format("Equal Left: %s, Right:%s", l, r));
