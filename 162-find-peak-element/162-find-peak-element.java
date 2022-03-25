fea9d0256631201935aa91769d220247366f35d2
rkk <30936750+rohankayan@users.noreply.github.com>
2022-03-25 13:56:58 -0700

    Time: 0 ms (100.00%), Space: 42.7 MB (63.51%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,12 +1,23 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         
-        for(int i =0; i< nums.length-1;i++){
-            if(nums[i] > nums[i+1])
-                return i;
+       return binarySearch(nums,0,nums.length-1);
+        
+    }
+    
+    
+    int binarySearch(int[] nums, int l, int r){
+        if(l==r){
+            return l;
         }
         
-        return nums.length-1;
+        int mid = l + (r-l)/2;
         
+        if(nums[mid]<nums[mid+1]){
+            return binarySearch(nums,mid+1,r);
+            
+        }
+        
+        return binarySearch(nums,l,mid);
     }
 }
