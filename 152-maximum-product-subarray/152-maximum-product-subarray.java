8bbbf0a9d4600a53d69bb752f318945dc7a3db93
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-26 16:03:53 -0700

    Time: 206 ms (8.13%), Space: 54.8 MB (5.06%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -0,0 +1,41 @@
+class Solution {
+    int maxProduct;
+    int minProduct;
+    
+    public int maxProduct(int[] nums) {
+        
+        maxProduct = nums[0];
+        
+        minProduct = nums[0];
+        
+        int ans = nums[0];
+        
+        for(int i =1; i<  nums.length ; i++){
+            
+            System.out.println("Before swap MaxProduct:"+ maxProduct +",minProduct:"+ minProduct +",num:"+nums[i]);
+            if(nums[i] < 0){                
+                swap(maxProduct,minProduct);
+            }
+            
+            System.out.println("MaxProduct:"+ maxProduct +",minProduct:"+ minProduct +",num:"+nums[i]);
+            
+            maxProduct = Math.max(maxProduct*nums[i],nums[i] );
+            minProduct = Math.min(minProduct*nums[i],nums[i] );
+            
+            
+            ans = Math.max(maxProduct, ans);
+            
+            
+        }
+        
+        return ans;
+        
+    }
+    
+    
+    private void swap(int max, int min){        
+        minProduct = max;
+        maxProduct = min;
+        
+    }
+}
