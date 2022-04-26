4e1a3c68fbad0026e356ebfe3a17ca4434a1cceb
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-26 13:19:29 -0700

    Time: 2 ms (88.06%), Space: 42.4 MB (95.68%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,79 +1,59 @@
 class Solution {
     
-    int[][] directions = {{-1,0},{0,-1},{1,0},{0,1}};
+   int[][] directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     int m = 0;
     int n = 0;
+
     public int closedIsland(int[][] grid) {
-        
         m = grid.length;
         n = grid[0].length;
-        int result = 0;  
-        /**
-        
-        for(int i=0;i<m;i++){            
-            for(int j = 0; j<n ; j++){                
-                if(i==0 || j==0 || i == m-1 || j ==n-1){                    
-                    if(grid[i][j]==0){
-                        fill(grid,i,j);
-                    }
+        int result = 0;
+
+        int[] iBorders = {0, m - 1};
+        int[] jBorders = {0, n - 1};
+
+        for (int i : iBorders) {
+            for (int j = 0; j < n; j++) {
+
+                fill(grid, i, j);
+            }
+        }
+
+        for (int j : jBorders) {
+            for (int i = 0; i < m; i++) {
+
+                fill(grid, i, j);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == 0) {
+                    result++;
+                    fill(grid, i, j);
                 }
             }
         }
-        **/
-        
-         int[] iBorders = {0,m-1};
-        
-         int[] jBorders = {0,n-1};
-        
-        for(int i: iBorders){
-            for(int j = 0; j<n ; j++){                
-                
-                        fill(grid,i,j);
-                    }         
-            
-            
-        }
-        
-         for(int j: jBorders){
-            for(int i = 0; i<m ; i++){                
-                
-                        fill(grid,i,j);
-                    }         
-            
-            
-        }
-        
-         for(int i=0;i<m;i++){
-            
-            for(int j = 0; j<n ; j++){
-                
-                                   
-                    if(grid[i][j]==0){
-                        result ++;
-                        fill(grid,i,j);
-                    }
-                
-            }
-        }
-        
+
         return result;
-        
     }
-    
-    
-    private void fill(int[][] g, int i, int j){
-          
-        if(i<0 || j<0 || i >=m ||j>=n )
+
+
+    private void fill(int[][] g, int i, int j) {
+
+        if (i < 0 || j < 0 || i >= m || j >= n) {
             return;
-        if(g[i][j] == 1)
-            return;
-        g[i][j] = 1;
-        
-        for(int[] d : directions){
-            
-            fill(g, i + d[0], j +d[1]);
         }
-        
+        if (g[i][j] == 1) {
+            return;
+        }
+        g[i][j] = 1;
+
+        for (int[] d : directions) {
+            fill(g, i + d[0], j + d[1]);
+        }
+
     }
     
     
