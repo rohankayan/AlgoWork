a56b83980cbc8740f8ce16f4b02d6530c6772df9
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-27 20:59:23 -0700

    Time: 3 ms (53.75%), Space: 46.4 MB (20.46%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -5,16 +5,16 @@ class Solution {
         
         for (int[] row : dp)
             Arrays.fill(row, Integer.MAX_VALUE);
+        //Base condition
+        
         dp[0][1] =0;
         dp[1][0] =0;
         
         for(int i = 0 ; i < grid.length; i++){            
-            for(int j = 0 ; j < grid[0].length; j++){                
-             
-                    dp[i+1][j+1] = Math.min(dp[i][j+1], dp[i+1][j]) + grid[i][j];
+            for(int j = 0 ; j < grid[0].length; j++){             
+             dp[i+1][j+1] = Math.min(dp[i][j+1], dp[i+1][j]) + grid[i][j];
             }
-        }
-        
+        }       
         
         return dp[grid.length][grid[0].length];
         
