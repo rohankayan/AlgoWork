0f8cd47dc891676333ebcfca73e645d4090387fc
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-27 21:31:45 -0700

    Time: 30 ms (56.06%), Space: 78.5 MB (81.21%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -0,0 +1,48 @@
+class Solution {
+    
+    int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+    
+    
+    
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+    
+        int result =0;
+    for(int i =0;i< grid2.length; i++){
+        for(int j = 0; j< grid2[0].length; j++){
+            if(grid2[i][j] == 1 && grid1[i][j] == 1){
+               
+                
+                
+                
+                result +=markAndCheckSubIsland(grid1,grid2,i,j);
+                //System.out.println(String.format("i:%d, j:%d, result: %d", i, j, result));
+            }
+        }
+    }
+        
+        return result;
+        
+    }
+    
+     int markAndCheckSubIsland(int[][] grid1, int[][] grid2, int i, int j){
+        
+        if( i < 0 || j <0 || i== grid2.length || j == grid2[0].length || grid2[i][j] != 1 )
+            return 1;
+        if(grid2[i][j] == 1 && grid1[i][j] != 1){
+            //System.out.println(String.format("Returning zero for i:%d, j:%d", i, j));
+            return 0;
+        }
+            
+        
+        grid2[i][j] =2;
+        int result =1;
+        for(int[] d : directions){           
+            
+            result *=markAndCheckSubIsland(grid1,grid2,i+d[0],j+d[1]);
+        }
+        
+         
+        return result;
+        
+    }
+}
