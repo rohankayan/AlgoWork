7a4fe92f2ac3b644cd25d2b13de3f5b8bd674350
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-27 21:56:14 -0700

    Time: 36 ms (37.08%), Space: 128.7 MB (36.28%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -9,7 +9,7 @@ class Solution {
         int result =0;
     for(int i =0;i< grid2.length; i++){
         for(int j = 0; j< grid2[0].length; j++){
-            if(grid2[i][j] == 1 && grid1[i][j] == 1){
+            if(grid2[i][j] == 1 ){
                 result +=markAndCheckSubIsland(grid1,grid2,i,j);                
             }
         }
@@ -23,11 +23,11 @@ class Solution {
         
         if( i < 0 || j <0 || i== grid2.length || j == grid2[0].length || grid2[i][j] != 1 )
             return 1;
-        if(grid2[i][j] == 1 && grid1[i][j] != 1){           
+        if( grid1[i][j] != 1){           
             return 0;
         }            
         
-        grid2[i][j] =2;
+        grid2[i][j] =0;
         int result =1;
         for(int[] d : directions){        
             result *=markAndCheckSubIsland(grid1,grid2,i+d[0],j+d[1]);
