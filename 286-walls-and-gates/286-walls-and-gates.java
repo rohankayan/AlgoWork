b9dee402cd9351d6cff92f0f44a18df69004fd22
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-28 21:25:59 -0700

    Time: 9 ms (73.80%), Space: 57.7 MB (21.63%) - LeetHub

---
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+    int INF = Integer.MAX_VALUE;
+    
+    public void wallsAndGates(int[][] rooms) {
+        
+        for(int i=0;i< rooms.length;i++){
+            for(int j=0;j< rooms[0].length;j++){
+                
+                if(rooms[i][j]==0){                    
+                    markDistance(rooms, i+1, j, 1);
+                    markDistance(rooms, i, j+1, 1);
+                    markDistance(rooms, i-1, j, 1);
+                    markDistance(rooms, i, j-1, 1);
+                }
+            }
+        }
+        
+    }
+    
+    private void markDistance(int[][] rooms, int i , int j, int distance){
+        
+        if(i<0 || j< 0|| i== rooms.length || j== rooms[0].length || rooms[i][j] == -1)
+            return;
+        
+        if(rooms[i][j] > distance ){
+            
+            rooms[i][j] = distance;
+            //System.out.println(String.format("i:%d,j:%d,distance:%d", i, j, distance));
+            for(int [] d: directions){                
+                markDistance(rooms, i+d[0], j+d[1], distance+1);
+        }
+            
+        }
+        
+       
+        
+        
+        return;
+        
+    }
+}
