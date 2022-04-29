9bd19c694b812bcd05705d4c47a6d203a10edc63
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-28 22:12:37 -0700

    Time: 1 ms (99.87%), Space: 44.8 MB (49.84%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -2,17 +2,14 @@ class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int[] passengerCount =new int[1001];
         
-        for(int[] t: trips){
-            
+        for(int[] t: trips){            
             passengerCount[t[1]] += t[0];
             passengerCount[t[2]] -= t[0];            
         }
         
         int currentCapacity =0;
-        for(int i=0;i< 1001;i++){
-            
-            currentCapacity += passengerCount[i];
-            
+        for(int i=0;i< 1001;i++){            
+            currentCapacity += passengerCount[i];            
             if(currentCapacity> capacity)
                 return false;
         }
