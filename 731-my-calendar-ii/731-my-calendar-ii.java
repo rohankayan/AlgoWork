e1ccced52ace5c4dec3d9df397c6bfdb20209fa6
rkk <30936750+rohankayan@users.noreply.github.com>
2022-04-28 22:34:04 -0700

    Time: 413 ms (20.62%), Space: 54.8 MB (39.29%) - LeetHub

---
+++ 731-my-calendar-ii/731-my-calendar-ii.java
@@ -0,0 +1,36 @@
+class MyCalendarTwo {
+    
+    TreeMap<Integer,Integer> timeCount = new TreeMap();
+    public MyCalendarTwo() {
+        
+    }
+    
+    public boolean book(int start, int end) {
+       timeCount.put(start, timeCount.getOrDefault(start,0)+1);
+        timeCount.put(end, timeCount.getOrDefault(end,0)-1);
+        
+        int activeBooking =0;
+        
+        for(Map.Entry<Integer,Integer> e: timeCount.entrySet()){
+            
+            activeBooking += e.getValue();
+            
+            if(activeBooking >=3){
+                timeCount.put(start, timeCount.getOrDefault(start,0)-1);
+                timeCount.put(end, timeCount.getOrDefault(end,0)+1);
+                
+                return false;
+                
+            }
+        }
+        
+        return true;
+        
+    }
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
