125783fabfd8eb361e1947dcff5985ba8199f55a
rkk <30936750+rohankayan@users.noreply.github.com>
2022-06-07 18:29:33 -0700

    Time: 0 ms (100.00%), Space: 42.5 MB (83.62%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -3,23 +3,26 @@ class Solution {
         
         List<List<Integer>>  results = new ArrayList<>();
         
-            backTrack(results, new ArrayList<>(), nums, 0);
+        dfs(results, new ArrayList<>(), nums, 0);
         
         return results;
         
     }
     
-    void backTrack(List<List<Integer>>  results, List<Integer> currentSq,int[] nums, int i ){
+    void dfs(List<List<Integer>>  results, List<Integer> currentSq,int[] nums, int i ){
         
-       if(i> nums.length)
+       if(i>= nums.length)
+       {
+           results.add(new ArrayList(currentSq));
            return;
-        results.add(new ArrayList(currentSq));
+       }
+           
+        currentSq.add(nums[i]);
+        dfs(results, currentSq, nums, i+1);
+        currentSq.remove(currentSq.size()-1);
+        dfs(results, currentSq, nums, i+1);
           
-        for(int j=i; j<= nums.length-1;j++){
-            currentSq.add(nums[j]);
-            backTrack(results, currentSq, nums, j+1);
-            currentSq.remove(currentSq.size()-1);
-        }
+       
            
        }
     
