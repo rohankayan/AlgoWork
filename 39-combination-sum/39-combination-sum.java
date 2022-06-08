de8fe5a9410a2a66cee64988387b4d88d651b178
rkk <30936750+rohankayan@users.noreply.github.com>
2022-06-07 18:35:42 -0700

    Time: 4 ms (79.78%), Space: 42.3 MB (96.16%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,34 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<List<Integer>>();
-        LinkedList<Integer> comb = new LinkedList<Integer>();
-        Arrays.sort(candidates);
-
-        this.backtrack( comb,candidates, target, 0, results);
+        List<List<Integer>> results = new ArrayList<>();
+        
+        backtrack(results, new ArrayList(), 0, target, candidates,0);
         return results;
     }
     
-    boolean backtrack(LinkedList<Integer> combination,int[] candidates, int remain, int start, List<List<Integer>> result){
+    
+    void backtrack(List<List<Integer>> results, List<Integer> currentSq, int currentSum, int target, int[] candidates, int i){
+        
+        if(currentSum == target){
+            results.add(new ArrayList(currentSq));
+            return;
+        }
+        
+        if(currentSum > target){
+            return;
+        }
+        
+        for(int j=i;j< candidates.length;j++){
+            currentSq.add(candidates[j]);            
+            backtrack(results, currentSq, currentSum+candidates[j], target, candidates,j);
+            currentSq.remove(currentSq.size()-1);            
+        }
         
         
-        if(remain ==0){
-            result.add(new ArrayList<Integer>(combination));
-            return true;
-        }
-        if(remain < 0){
-            return true;
-        }
-        boolean amIdoneWithThisLoop = false;
-        for(int i = start ; i< candidates.length && !amIdoneWithThisLoop; i++){
-            combination.add(candidates[i]);
-            //System.out.println(combination);
-            amIdoneWithThisLoop = this.backtrack(combination, candidates, remain-candidates[i], i , result);
-            
-            combination.removeLast();
-            
-        }
-        return false;
     }
 }
-
-// n ^ (target/smallestnumber)(saving some with pruning) + nlog(n)
