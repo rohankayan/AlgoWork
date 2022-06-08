4f7ea427c88a06157fe9dbb119e371bc82910244
rkk <30936750+rohankayan@users.noreply.github.com>
2022-06-07 18:11:09 -0700

    Time: 0 ms (100.00%), Space: 42.5 MB (78.99%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>>  results = new ArrayList();
         
-            backtracking(nums, new ArrayList(), 0, results);
+        List<List<Integer>>  results = new ArrayList<>();
+        
+            backTrack(results, new ArrayList<>(), nums, 0);
+        
         return results;
         
     }
     
-    void backtracking(int[] nums, List<Integer> currentSubset, int first, List<List<Integer>>  results){
+    void backTrack(List<List<Integer>>  results, List<Integer> currentSq,int[] nums, int i ){
         
-        if(currentSubset.size() > nums.length){
-            return;
+       if(i> nums.length)
+           return;
+        results.add(new ArrayList(currentSq));
+          
+        for(int j=i; j<= nums.length-1;j++){
+            currentSq.add(nums[j]);
+            backTrack(results, currentSq, nums, j+1);
+            currentSq.remove(currentSq.size()-1);
         }
-        results.add(new ArrayList(currentSubset));
-               
-        
-        for(int j = first; j< nums.length; j++){
-             currentSubset.add(nums[j]);       
-        
-             backtracking(nums, currentSubset, j+1, results);
-        
-            currentSubset.remove(currentSubset.size()-1);
-        }
-        
-        
-        
-    }
+           
+       }
+    
 }
