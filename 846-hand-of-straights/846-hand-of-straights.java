a4ffe14a8d838a70f9ab9696d580e6cad6a0ec38
rkk <30936750+rohankayan@users.noreply.github.com>
2022-07-14 14:49:11 -0700

    Time: 68 ms (59.98%), Space: 59.3 MB (11.72%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.java
@@ -0,0 +1,47 @@
+class Solution {
+     public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        
+        if(hand.length%groupSize != 0)
+            return  false;
+        
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+
+        Map<Integer, Integer> numCount = new HashMap<Integer, Integer>();
+
+
+        for(int h : hand){
+            int currCount = numCount.getOrDefault(h,0);
+            currCount++;
+            numCount.put(h,currCount);
+            heap.add(h);
+        }
+
+        while(!heap.isEmpty()){
+            
+            Integer smallest = heap.peek();
+            int currCount = numCount.get(smallest);
+            if(currCount != 0){
+                for (int i =0; i<groupSize;i++){
+                    if(numCount.containsKey(smallest+i))
+                    {
+                        currCount = numCount.get(smallest+i);
+                        if(currCount <= 0){
+                            return false;
+                        }
+                        numCount.put(smallest+i,--currCount);
+                    }
+                    else{
+                        return false;
+                    }
+
+                }
+            }
+            
+            heap.poll();
+        }
+        return true;
+
+    }
+}
