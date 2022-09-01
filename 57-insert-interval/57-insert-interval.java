c4feb2112cda3fa0f3fbced7921f22b677050bae
rkk <30936750+rohankayan@users.noreply.github.com>
2022-09-01 09:45:41 -0700

    Time: 3 ms (43.61%), Space: 47.9 MB (30.59%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -2,34 +2,37 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         
         
-         List<int[]> result = new ArrayList<>(); 
-
-        for(int[] interval : intervals){
-            if(newInterval == null ||  interval[1]< newInterval[0]){
+        List<int[]> result = new ArrayList<int[]>();
+        // if newIntervale is null 
+        
+        //Keep the merged range interval as new interval 
+        
+        for(int[] interval : intervals ){
+            
+            
+            if(newInterval == null || interval[1]< newInterval[0]){
                 result.add(interval);
             }
-            else if( interval[0] > newInterval[1] ){
-
+            else if(newInterval[1]< interval[0]){
+                
                 result.add(newInterval);
                 result.add(interval);
                 newInterval = null;
             }
-            else{
-                newInterval[0] = Math.min(interval[0],newInterval[0]);
-                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            else {
+                newInterval[0] = Math.min(newInterval[0],interval[0] );
+                newInterval[1] = Math.max(newInterval[1],interval[1] );
+                
             }
-
-        }
-
+                
+                
+            }
+        
         if(newInterval != null){
             result.add(newInterval);
         }
-
-
-
-       return result.toArray(new int[result.size()][2]);
         
-        
-        
-    }
+        return result.toArray(new int[result.size()][2]);
+        }
+    
 }
