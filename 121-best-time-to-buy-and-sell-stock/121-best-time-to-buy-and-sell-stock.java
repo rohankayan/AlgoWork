cadae750b2583ed9a175e9b495b1cd4bff9886d6
rkk <30936750+rohankayan@users.noreply.github.com>
2022-09-02 09:04:50 -0700

    Time: 5 ms (12.48%), Space: 84.3 MB (20.65%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        
+        // if sell price is more than purchase price sell the stock and see the profit 
+        // keep minimum purchase price all the time and keep updating the profit
+        
+        
+        int minPurchasePrice = Integer.MAX_VALUE;
+        int maxProfit = Integer.MIN_VALUE;
+        
+        for(int p: prices){
+            int tempProfit = 0;
+            if(p< minPurchasePrice)
+                minPurchasePrice=p;
+            if(p > minPurchasePrice)
+                tempProfit = p - minPurchasePrice;
+            maxProfit = Math.max(maxProfit,tempProfit);
+            
+        }
+        
+        return maxProfit;
+    }
+}
