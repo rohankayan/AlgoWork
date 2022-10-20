b375fa98215e4d66134d79d207e4e474b11381ab
rkk <30936750+rohankayan@users.noreply.github.com>
2022-10-19 20:33:47 -0700

    Time: 8 ms (38.86%), Space: 58.5 MB (5.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,41 +1,28 @@
 class Solution {
+    int numOfIsland = 0;   
+    int[][] neighbors ={{0,-1},{0,1},{-1,0},{1,0}};
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int numOfIsland = 0;
-        // 0 = water , 1 = land
-        
-        for(int i = 0; i < m ; i++){
-            
-            for (int j = 0 ; j < n ; j++){
-                
-                if(grid[i][j] == '1'){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] =='1'){
                     numOfIsland++;
-                    bfs(grid, i, j , m ,n);
+                    dfs(i,j,m,n,grid);
                 }
-                
-                
             }
         }
-        
         return numOfIsland;
-        
     }
-    
-    private void bfs(char[][] grid , int startI , int startJ, int m , int n){
-        
-        if(startI < 0 || startJ < 0 || startI >= m || startJ >= n ){
+
+    void dfs(int i, int j, int m, int n, char[][] grid){
+        if(i<0 || i >= m || j<0 || j >=n || grid[i][j] !='1'){
             return;
         }
+        grid[i][j] ='2';
         
-        if(grid[startI][startJ] != '1')
-            return;
-        
-        grid[startI][startJ] = '0';
-        
-        bfs(grid, startI+1, startJ , m ,n);
-        bfs(grid, startI-1, startJ , m ,n);
-        bfs(grid, startI, startJ+1 , m ,n);
-        bfs(grid, startI, startJ-1 , m ,n);
+        for(int[] ng:neighbors){
+            dfs(i+ng[0],j+ng[1],m,n,grid);
+        }
     }
 }
