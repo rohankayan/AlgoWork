c9f6b410c56e01c24f6ea6a1ff844ac9f2adafe2
rkk <30936750+rohankayan@users.noreply.github.com>
2022-11-01 12:16:24 -0700

    Time: 3 ms (57.06%), Space: 44 MB (56.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,9 +1,9 @@
 class Solution {
         public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Set<List<Integer>> results = new HashSet<>();
-            Arrays.sort(nums);
-        backtracking(0, new ArrayList<>(), nums, nums.length, results);
-        return new ArrayList<>(results);
+        List<List<Integer>> results = new ArrayList<>();
+        Arrays.sort(nums);
+        backtracking2(0, new ArrayList<>(), nums, nums.length, results);
+        return results;
     }
 
     void backtracking( int index , List<Integer> currSeq, int[] nums, int length, Set<List<Integer>> results){
@@ -17,5 +17,23 @@ class Solution {
         backtracking(index+1, currSeq, nums, length, results);
         currSeq.remove(currSeq.size()-1);
         backtracking(index+1, currSeq, nums, length, results);
+    }
+    
+     void backtracking2( int index , List<Integer> currSeq, int[] nums, int length, List<List<Integer>> results){
+
+
+        results.add(new ArrayList<>(currSeq));
+
+
+        for(int i = index; i < length; i++){
+            if(i != index && nums[i]== nums[i-1])
+                continue;
+            currSeq.add(nums[i]);
+            backtracking2(i+1, currSeq, nums, length, results);
+            currSeq.remove(currSeq.size()-1);
+        }
+
+
+        
     }
 }
